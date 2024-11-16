8608fe413311a21cefd296b01e3e17e078f54327
Karthikeya Panangipalli <karthikeya.panangipalli.02@gmail.com>
2024-11-16 05:12:26 -0800

    Input complete phone number

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generateSignature() {
             <div class="pronouns">${pronouns}</div>
             <div class="spacer"></div>
             <div class="address">901 12th Avenue, Seattle, WA 98122-1090</div>
-            <div class="phone">Office: (206) 296-${phone} | <a href="mailto:${email}">${email}</a></div>
+            <div class="phone">Office: ${phone} | <a href="mailto:${email}">${email}</a></div>
         </div>
     `;
 
